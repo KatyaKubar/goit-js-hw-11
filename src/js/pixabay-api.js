9ec20d89384f2f3c6a56caec36b71f254e6a2c1b
KatyaKubar <116179784+KatyaKubar@users.noreply.github.com>
2024-02-14 20:11:41 +0200

    Summary

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,1 @@
-import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = '42372346-6df3db87eb4ada5723d622fbb';
-
-export async function fetchPhoto(q, page, perPage) {
-  try {
-    const url = `${BASE_URL}/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.log(error.message);
-  }
-}
